f052b19ad487b82b1dbd8461a32bfd08fe5560f0
Federico Torrielli <evilscript@protonmail.com>
2026-09-10 17:36:34 +0200

    update author link

--- src/MadeByMe.tsx
+++ src/MadeByMe.tsx
@@ -1,7 +1,7 @@
 function MadeByMe() {
     return (
         <div className="absolute bottom-5 left-5 text-md mt-4">Made with 🧠 by <a
-            href="https://evilscript.eu">Federico
+            href="https://federicotorrielli.me">Federico
             Torrielli</a>
         </div>
     );
